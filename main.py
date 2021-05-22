f2dd5674f73cf3023490f68e82983440fef65c26
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-05-22 16:25:31 +0200

    Streamlit setup

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+from core.utils.financial_plots import *
+from core.utils.financial_data import *
+
+st.title('FORECASTING')
+
+ticker = 'AAPL'
+company_name = 'APPLE'
+close_prices = get_data(ticker)
+
+st.plotly_chart(plot_historical_price(ticker,company_name,close_prices))
